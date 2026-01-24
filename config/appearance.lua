332f82241a3bdfa9f16f419f56b34889cab2c170
github_action <ci_email@yolobuyer.com>
2026-01-24 17:18:18 +0800

    refactor

--- config/appearance.lua
+++ config/appearance.lua
@@ -1,6 +1,49 @@
 local wezterm = require("wezterm") ---@type Wezterm
 local M = {}
 
+local function config_base_style(config)
+    -- =========== 基础外观 ===========
+    -- config.window_background_opacity = 0.4
+    -- config.macos_window_background_blur = 80
+    config.window_close_confirmation = "NeverPrompt"
+    config.default_cursor_style = "BlinkingBar"
+    config.font_size = 24
+    -- config.color_scheme = "Marrakesh (light) (terminal.sexy)"
+    -- config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
+    -- config.color_scheme = 'Material'
+    -- config.color_scheme = 'MaterialDesignColors'
+    -- config.color_scheme = 'nord-light'
+    -- config.color_scheme = 'Medallion'
+    -- config.color_scheme = 'Mocha (dark) (terminal.sexy)'
+    -- config.color_scheme = 'Sat (Gogh)'
+    config.color_scheme = 'Seafoam Pastel'
+
+    config.window_decorations = "RESIZE|MACOS_FORCE_SQUARE_CORNERS"
+
+    --     config.window_frame = {
+    --         border_left_width = '0.5cell',
+    --         border_right_width = '0.5cell',
+    --         border_bottom_height = '0.25cell',
+    --         border_top_height = '0.25cell',
+    --         border_left_color = 'purple',
+    --         border_right_color = 'purple',
+    --         border_bottom_color = 'purple',
+    --         border_top_color = 'purple',    
+    --    }
+    config.window_padding = {
+        left = 30,
+        right = 30,
+        top = 30,
+        bottom = 30
+    }
+    -- local border_width = "2px";
+    -- local border_color = "#C06DD8";
+    -- config.window_frame = {
+    --     border_left_width = 10,
+    --     border_right_width = 10
+    -- }
+end
+
 local function config_tab_bar_style(config)
     -- config.use_fancy_tab_bar = false
     config.underline_thickness = "2px"
@@ -59,46 +102,7 @@ local function config_cammand_palette_style(config)
 end
 
 function M.setup(config)
-    -- =========== 基础外观 ===========
-    -- config.window_background_opacity = 0.4
-    -- config.macos_window_background_blur = 80
-    config.window_close_confirmation = "NeverPrompt"
-    config.default_cursor_style = "BlinkingBar"
-    config.font_size = 24
-    -- config.color_scheme = "Marrakesh (light) (terminal.sexy)"
-    -- config.color_scheme = 'Monokai Pro Ristretto (Gogh)'
-    -- config.color_scheme = 'Material'
-    -- config.color_scheme = 'MaterialDesignColors'
-    -- config.color_scheme = 'nord-light'
-    -- config.color_scheme = 'Medallion'
-    -- config.color_scheme = 'Mocha (dark) (terminal.sexy)'
-    -- config.color_scheme = 'Sat (Gogh)'
-    config.color_scheme = 'Seafoam Pastel'
-
-    config.window_decorations = "RESIZE|MACOS_FORCE_SQUARE_CORNERS"
-
-    --     config.window_frame = {
-    --         border_left_width = '0.5cell',
-    --         border_right_width = '0.5cell',
-    --         border_bottom_height = '0.25cell',
-    --         border_top_height = '0.25cell',
-    --         border_left_color = 'purple',
-    --         border_right_color = 'purple',
-    --         border_bottom_color = 'purple',
-    --         border_top_color = 'purple',    
-    --    }
-    config.window_padding = {
-        left = 30,
-        right = 30,
-        top = 30,
-        bottom = 30
-    }
-    -- local border_width = "2px";
-    -- local border_color = "#C06DD8";
-    -- config.window_frame = {
-    --     border_left_width = 10,
-    --     border_right_width = 10
-    -- }
+    config_base_style(config)
     config_tab_bar_style(config)
     config_cammand_palette_style(config)
 end
